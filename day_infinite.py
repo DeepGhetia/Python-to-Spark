8b4928faf5d763b8718e1982fd2c2719e0006ec9
DeepGhetia <17cs030.deep@sjec.ac.in>
2026-01-17 15:19:14 +0530

    new additions in Hive Logic

--- day_infinite.py
+++
@@ -1,6 +0,0 @@
-from functools import reduce
-lt = [1,2,3,4,5]
-
-print(list(map(lambda x: x%2==0,lt))) #transformation
-print(list(filter(lambda x: x%2==0,lt))) #filtering (select/project)
-print(reduce(lambda x,y:x*y,lt)) #aggregation
