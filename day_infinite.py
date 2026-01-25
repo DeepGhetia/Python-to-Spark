8ebbe7de7dde7e797837055e34c8d838eac158a8
DeepGhetia <17cs030.deep@sjec.ac.in>
2026-01-25 14:59:03 +0530

    new updates to delta file

--- day_infinite.py
+++
@@ -1,9 +0,0 @@
-s = '''Name~|Age
-Brayan,gomez~|25
-John,Cleark~|30
-Sumit,Sen~|31
-'''
-lt = list(map(lambda x: x.replace('~',''),s.splitlines()[1:]))
-with open('C:/Users/hp/Desktop/python/Python-to-Spark/read.txt','wt') as file:
-    for i in lt:
-        file.write(f'{i}\n')
