a9469e460d75a5d94ff600b088b3ed71f8ed1220
DeepGhetia <17cs030.deep@sjec.ac.in>
2026-06-15 17:51:16 +0530

    new changes

--- day_infinite.py
+++ day_infinite.py
@@ -1,6 +1,6 @@
 lt = [1,2,3,4,5,6]
-# print([(lt[i],lt[j],lt[k]) for i in range(len(lt)) for j in range(i+1,len(lt)) for k in range(j+1,len(lt))])
-# print([lt[i:i+2] for i in range(0,len(lt),2)])
-# for i,v in enumerate(lt,start=0):
-#     print(i,v)
+print([(lt[i],lt[j],lt[k]) for i in range(len(lt)) for j in range(i+1,len(lt)) for k in range(j+1,len(lt))])
+print([lt[i:i+2] for i in range(0,len(lt),2)])
+for i,v in enumerate(lt,start=0):
+    print(i,v)
 
